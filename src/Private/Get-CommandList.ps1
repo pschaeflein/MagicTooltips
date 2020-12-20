c2a739997d1f4a90fc05105d7428afda532fae27
TravisTX <travistx@gmail.com>
2020-12-20 09:01:44 -0700

    ✨ allow customizing commands

--- src/Private/Get-CommandList.ps1
+++ src/Private/Get-CommandList.ps1
@@ -1,26 +1,49 @@
 function Get-CommandList() {
-    $commandList = @{
-        "kubectl.exe"   = "kubernetes"
-        "helm.exe"      = "kubernetes"
-        "az.cmd"        = "azure"
-        "terraform.exe" = "azure"
-    }
+    $commandList = @{}
 
-    $keys = @() + $commandList.Keys
-    foreach ($key in $keys) {
-        $alias = $key.replace(".exe", "").replace(".cmd", "");
-        if ($key -ne $alias) {
-            $commandList.Add($alias, $commandList[$key])
+    function Add-Command($command, $provider) {
+        .$context.WriteLog "$command = $provider"
+        if (!$commandList.ContainsKey($command)) {
+            $commandList.Add($command, $provider)
         }
     }
 
-    $keys = @() + $commandList.Keys
-    foreach ($key in $keys) {
-        $aliases = (get-alias).Where( { $_.Definition -eq $key }).Name;
-        foreach ($alias in $aliases) {
-            $commandList.Add($alias, $commandList[$key])
+    function Add-Commands($provider, $defaultCommands) {
+        .$Context.WriteLog "Add-Commands $provider"
+        $commands = $defaultCommands
+        $customCommands = Get-Variable "MagicTooltips_$($provider)Commands" -ValueOnly -ErrorAction SilentlyContinue
+
+        if ($null -ne $customCommands) {
+            .$Context.WriteLog "Using custom commands"
+            $commands = $customCommands
+        }
+    
+        foreach ($command in $commands.split(',')) {
+            Add-Command $command $provider
         }
     }
 
+    function Add-Aliases() {
+        .$Context.WriteLog "Add-Aliases"
+        $keys = @() + $commandList.Keys
+        foreach ($key in $keys) {
+            $alias = $key.replace(".exe", "").replace(".cmd", "");
+            if ($key -ne $alias) {
+                Add-Command $alias $commandList[$key]
+            }
+        }
+    
+        $keys = @() + $commandList.Keys
+        foreach ($key in $keys) {
+            $aliases = (get-alias).Where( { $_.Definition -eq $key }).Name;
+            foreach ($alias in $aliases) {
+                Add-Command $alias $commandList[$key]
+            }
+        }
+    }
+
+    Add-Commands "kubernetes" "kubectl,helm,kubens,kubectx,oc,istioctl,kogito,k9s,helmlist"
+    Add-Commands "azure" "az,terraform,pulumi,terragrunt"
+    Add-Aliases
     return $commandList
 }
